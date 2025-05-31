39e8206ea7d3ed68c2b9741e720b76c6044031b3
abir <abir196600@gmail.com>
2025-05-31 15:26:17 +0600

    little change

--- frontend/Task_Manager/src/utils/apiPaths.js
+++ frontend/Task_Manager/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_API_URL;
+export const BASE_URL = "https://task-manager-website-backend.onrender.com"; // Base URL for the API
 
 // utils/apiPaths.js
 export const API_PATHS = {
